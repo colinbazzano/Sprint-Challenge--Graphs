b9a36dcb34b976b41b0aff6eaf5b0d7747b6aca5
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-05-15 09:20:00 -0400

    tests pass 998 moves to traverse maze

--- adv.py
+++ adv.py
@@ -5,6 +5,8 @@ from world import World
 import random
 from ast import literal_eval
 
+
+from util import Queue
 # Load world
 world = World()
 
@@ -27,7 +29,44 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+
+opposite_directions = {
+    "n": "s",
+    "s": "n",
+    "e": "w",
+    "w": "e"
+}
+
+
+def dft_recursive(starting_room):
+
+    def dft_helper(room, visited):
+        visited.add(room.id)  # add the first room
+        path = []  # keep track of our path
+
+        for direction in room.get_exits():  # appends available directions if direction_to is not None
+            # returns direction_to if there is one
+            next_room = room.get_room_in_direction(direction)
+            if next_room.id not in visited:  # if we have no visited the room in question
+                # call the helper on that room
+                next_room_path = dft_helper(next_room, visited)
+                if next_room_path:
+                    print("Path of next room:", next_room_path)
+                    new_path = [direction, *next_room_path,
+                                opposite_directions[direction]]
+                else:
+                    new_path = [direction, opposite_directions[direction]]
+                path = [*path, *new_path]
+        return path
+
+    visited = set()
+    path = dft_helper(starting_room, visited)
+    print("Path:", path)
+
+    return path
+
+
+traversal_path = dft_recursive(player.current_room)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
